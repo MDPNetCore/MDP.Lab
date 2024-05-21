fb9e18b53ff6ae5336c4cf9047ccda13e3988db3
clark159 <clark159@hotmail.com>
2024-05-22 03:39:02 +0800

    XXXXXXXXX

--- src2/MDP.BlazorCore.Authorization.Maui/Hosting/AuthorizationFactory.cs
+++ src2/MDP.BlazorCore.Authorization.Maui/Hosting/AuthorizationFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MDP.AspNetCore.Authorization;
 
 namespace MDP.BlazorCore.Authorization.Maui
 {
--- src2/MDP.BlazorCore.Authorization.Maui/Providers/BlazorAccessResourceProvider.cs
+++ src2/MDP.BlazorCore.Authorization.Maui/Providers/BlazorAccessResourceProvider.cs
@@ -1,33 +1,20 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MDP.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
 using System;
 
 namespace MDP.BlazorCore.Authorization.Maui
 {
     public class BlazorAccessResourceProvider : IAccessResourceProvider
     {
-        // Fields
-        private readonly NavigationManager _navigationManager = null;
-
-
-        // Constructors
-        public BlazorAccessResourceProvider(NavigationManager navigationManager)
-        {
-            #region Contracts
-
-            if (navigationManager == null) throw new ArgumentException($"{nameof(navigationManager)}=null");
-
-            #endregion
-
-            // Default
-            _navigationManager = navigationManager;
-        }
-
-
         // Methods
-        public AccessResource Create()
+        public AccessResource Create(object resource = null)
         {
+            // NavigationManager
+            var navigationManager = resource as NavigationManager;
+            if (navigationManager == null) return null;
+
             // Uri
-            var uriString = _navigationManager.Uri;
+            var uriString = navigationManager.Uri;
             if (string.IsNullOrEmpty(uriString) == true) return null;
             var uri = new Uri(uriString);
 
